4fc980d378879682782a309c8e9e445f2c928c0c
x0Thanawatx0x <tanami2077@gmail.com>
2026-03-25 18:00:24 +0700

    waitingPO

---
+++ SRC/Assets/Menu/C/Cutscene/CutsceneController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutsceneController : MonoBehaviour
+{
+    public Image cutsceneImage;
+    public Sprite[] cutsceneSprites;
+    
+
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        cutsceneImage.sprite = cutsceneSprites[currentIndex];
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            NextScene();
+        }
+    }
+
+    void NextScene()
+    {
+        currentIndex++;
+
+        if (currentIndex < cutsceneSprites.Length)
+        {
+            cutsceneImage.sprite = cutsceneSprites[currentIndex];
+        }
+        else
+        {
+            // ไป Scene ใหม่
+            SceneManager.LoadScene("GameScene"); // เปลี่ยนชื่อ Scene ตามที่ใช้
+        }
+    }
+}
